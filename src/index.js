171121151f04dfafb72d56bb8607ea04c7501cc2
Roberto Achar <robertoachar@gmail.com>
2018-04-18 21:13:01 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+module.exports.sum = (x, y) => {
+  return x + y;
+};
+
+module.exports.subtract = (x, y) => {
+  return x - y;
+};
+
+module.exports.multiply = (x, y) => {
+  return x * y;
+};
+
+module.exports.divide = (x, y) => {
+  return x / y;
+};
